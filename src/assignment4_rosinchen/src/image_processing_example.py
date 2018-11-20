07b607eb11ca1aa88e3a1330155575b9a6c4d3c7
jackinabox <adrian.defer@gmail.com>
2018-11-20 22:27:31 +0100

    cleaned out a bit

--- src/assignment4_rosinchen/src/image_processing_example.py
+++ src/assignment4_rosinchen/src/image_processing_example.py
@@ -22,136 +22,133 @@ class image_converter:
   def __init__(self):
     rospy.loginfo(" init class.")
 
-    self.image_pub = rospy.Publisher("/image_processing/bin_img",Image, queue_size=1)
-    #self.image_pubC = rospy.Publisher("/image_processing/bin_imgCenter",Image, queue_size=1)
-
+    self.image_grey_pub = rospy.Publisher("/image_processing/grey_img", Image, queue_size=1)
+    self.image_bin_pub = rospy.Publisher("/image_processing/bin_img", Image, queue_size=1)
+    self.image_dot_pub = rospy.Publisher("/image_processing/dot_img", Image, queue_size=1)
     self.bridge = CvBridge()
     #self.image_sub = rospy.Subscriber("/app/camera/rgb/image_raw",Image,self.callback, queue_size=1)
-    self.image_sub = rospy.Subscriber("/camera/color/image_raw",Image,self.callback, queue_size=1)
+    self.image_sub = rospy.Subscriber("/camera/color/image_raw", Image, self.callback, queue_size=1)
 
-  def solve(self,img_points):
+  def solve(self, img_points):
     img_points=np.array(img_points)
-    fx = 1367#614.1699
-    fy = 1367#614.9002
+    fx = 614.1699#1367
+    fy = 614.9002#1367
     cx = 329.9491
     cy = 237.2788
-    camera_mat = np.zeros((3,3,1))
-    camera_mat[:,:,0] = np.array([[fx, 0, cx],
+    camera_mat = np.zeros((3, 3, 1))
+    camera_mat[:, :, 0] = np.array([[fx, 0, cx],
         [0, fy, cy],
         [0, 0, 1]])
     k1 = 0.1115
     k2 = -0.1089
     p1 = 0
     p2 = 0
-    dist_coeffs = np.zeros((4,1))
-    dist_coeffs[:,0] = np.array([[k1, k2, p1, p2]])
+    dist_coeffs = np.zeros((4, 1))
+    dist_coeffs[:, 0] = np.array([[k1, k2, p1, p2]])
     # far to close, left to right (order of discovery) in cm
-    obj_points = np.zeros((6,3,1))
-    obj_points[:,:,0] = np.array([[58.5,00.0, 0],
-          [58.5,39.8, 0],
+    obj_points = np.zeros((6, 3, 1))
+    obj_points[:, :, 0] = np.array([[58.5, 0.0, 0],
+          [58.5, 39.8, 0],
           [28.0, 0.0, 0],
-          [29.0, 40.0,0],
+          [29.0, 40.0, 0],
           [0.0, 0.0,  0],
           [1.5, 40.0, 0]])
     rospy.loginfo("image:"+str(img_points))
-    rospy.loginfo("object:"+str(obj_points[:,:,0]))
-    retval, rvec, tvec = cv2.solvePnP(obj_points, img_points,camera_mat, dist_coeffs)
-    rmat = np.zeros((3,3))
-    rospy.loginfo("rvec,tvec:"+str(rvec)+" "+str(tvec))
+    rospy.loginfo("object:"+str(obj_points[:, :, 0]))
+    _, rvec, tvec = cv2.solvePnP(obj_points, img_points, camera_mat, dist_coeffs)
+    rmat = np.zeros((3, 3))
     rotation_matrix=cv2.Rodrigues(rvec, rmat, jacobian=0)[0]
-    rospy.loginfo("rotation matrix:"+str(rotation_matrix))
-    transformation=np.zeros((4,4))
-    transformation[0:3,0:3]=rotation_matrix
-    transformation[0:3,3]=tvec[:,0]
-    transformation[3,3]=1
-    rospy.loginfo("transformation matrix:"+str(transformation))
-    transformation_inv=np.linalg.inv(transformation)
-    rospy.loginfo("transformation matrix INV:"+str(transformation_inv))
-    #t_vec_real=np.matmul(transformation_inv,np.append(tvec[:,0],0))
-    vec_real=np.dot(transformation_inv,np.append([406.82318115, 583.26831055,0],1))
-    rospy.loginfo("vec real:"+str(vec_real))
-    
+    return (rvec, tvec, rotation_matrix)
+
     
   def callback(self,data):
-    rospy.loginfo("  start callback.")
+    rospy.loginfo("\n::: start callback :::")
 
     try:
       cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
 
-
     #make it gray
-    gray=cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+
+    try:
+      self.image_grey_pub.publish(self.bridge.cv2_to_imgmsg(gray, "mono8"))
+      rospy.loginfo("publishing grey image.")
+    except CvBridgeError as e:
+      print(e)
 
     #bi_gray
     bi_gray_max = 255
     bi_gray_min = 245
-    ret,thresh1=cv2.threshold(gray, bi_gray_min, bi_gray_max, cv2.THRESH_BINARY);
+    ret, img_binary = cv2.threshold(gray, bi_gray_min, bi_gray_max, cv2.THRESH_BINARY)
 
     #new----
     #rospy.loginfo("dim thres1 ret"+str(thresh1.shape)+str(ret))
-    dim=thresh1.shape
-    #thresh1=thresh1[0:int(0.85*dim[0]),:] #Car 125
-    #thresh1=thresh1[int(0.3*dim[0]):int(0.93*dim[0]),:] #Car 122
-    thresh1[0:int(0.3*dim[0])]=0 #Car 122
-    thresh1[int(0.93*dim[0]):dim[0],:]=0 #Car 122
+    dim = img_binary.shape
+    #img_binary = img_binary[0:int(0.85*dim[0]),:] #Car 125
+    img_binary[0:int(0.3*dim[0])] = 0 #Car 122
+    img_binary[int(0.93*dim[0]):dim[0], :] = 0 #Car 122
 
-    #gray=gray[int(0.3*dim[0]):int(0.93*dim[0]),:] #Car 122
+    try:
+      self.image_bin_pub.publish(self.bridge.cv2_to_imgmsg(img_binary, "mono8"))
+      rospy.loginfo("publishing binary image.")
+    except CvBridgeError as e:
+      print(e)
 
-    vectors=np.array(np.nonzero(thresh1)).T
+    vectors = np.array(np.nonzero(img_binary)).T
     rospy.loginfo("vectors"+str(vectors.shape))
     vectors = np.float32(vectors)
 
-    pointer=1
-    epsilon=20
-    clusterPointer=np.zeros(vectors.shape[0])
-    for vect in range(len(vectors)):
-      for vect2 in range(vect,len(vectors)):
-        if np.linalg.norm(vectors[vect]-vectors[vect2])<epsilon:
-          if clusterPointer[vect]!=0:
-            clusterPointer[vect2]=clusterPointer[vect]
+    numberOfClusters=1
+    epsilon = 20  # in pixels
+    clusterIndices = np.zeros(vectors.shape[0])
+    for vec_x in range(len(vectors)):
+      for vec_y in range(vec_x, len(vectors)):
+        if np.linalg.norm(vectors[vec_x]-vectors[vec_y]) < epsilon:
+          if clusterIndices[vec_x] != 0:
+            clusterIndices[vec_y] = clusterIndices[vec_x]
           else:
-            clusterPointer[vect]=pointer
-            clusterPointer[vect2]=pointer
-            pointer+=1
+            clusterIndices[vec_x] = numberOfClusters
+            clusterIndices[vec_y] = numberOfClusters
+            numberOfClusters += 1
 
-    #rospy.loginfo("clusterPointer"+str(clusterPointer))
-    #clusterVectors=[None for x in range(int(max(clusterPointer)))]
-    means=[None for x in range(int(max(clusterPointer)))]
-    for cluster in range(int(max(clusterPointer))):
+    #rospy.loginfo("clusterIndices"+str(clusterIndices))
+    #clusterVectors=[None for x in range(int(max(clusterIndices)))]
+
+    means = [None for x in range(int(max(clusterIndices)))]
+    for cluster in range(int(max(clusterIndices))):
       #clusterVectors[cluster]=vectors[clusterPointer==cluster+1]
-      means[cluster]=np.mean(vectors[clusterPointer==cluster+1],axis=0)
+      means[cluster] = np.mean(vectors[clusterIndices == cluster+1], axis=0)
       #rospy.loginfo("cluster "+str(cluster)+": "+str(means[cluster][:]))
 
     #rospy.loginfo("means"+str(means))
     #rospy.loginfo("clusterPointer mean"+str(means))
-    
-    replaceByCenter=True
-    if replaceByCenter:
-      #thresh2=np.zeros(thresh1.shape)#thresh1[:,:]
-      thresh1[:,:]=0 #w
-      for i in range(int(max(clusterPointer))):
-        thresh1[int(means[i][0]),int(means[i][1])]=255 #b
-    
-    self.solve(means)
 
-    #rospy.sleep(1)
+    img_binary[:, :] = 0
+    for i in range(int(max(clusterIndices))):
+      img_binary[int(means[i][0]), int(means[i][1])] = 255
 
+    try:
+      self.image_dot_pub.publish(self.bridge.cv2_to_imgmsg(img_binary, "mono8"))
+      rospy.loginfo("publishing centroids image.")
+    except CvBridgeError as e:
+      print(e)
 
+    rvec, tvec, rotation_matrix = self.solve(means)
+    rospy.loginfo("\nrvec,tvec:\n"+str(rvec)+"\n"+str(tvec))
+    rospy.loginfo("rotation matrix:\n"+str(rotation_matrix))
+    transformation=np.zeros((4, 4))
+    transformation[0:3, 0:3] = rotation_matrix
+    transformation[0:3, 3] = tvec[:, 0]
+    transformation[3, 3] = 1
+    rospy.loginfo("\ntransformation matrix:\n"+str(transformation))
+    transformation_inv = np.linalg.inv(transformation)
+    rospy.loginfo("\ntransformation matrix INV:\n"+str(transformation_inv))
+    #t_vec_real=np.matmul(transformation_inv,np.append(tvec[:,0],0))
+    vec_real = np.dot(transformation_inv, np.append([406.82318115, 583.26831055, 0], 1))
+    rospy.loginfo("vec real:"+str(vec_real))
 
-
-    #ret,labels,centers=cv2.kmeans(vectors, 6, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_MAX_ITER,1,10),
-    #       attempts=10, bestLabels=None, flags=cv2.KMEANS_RANDOM_CENTERS)
-    #centers=np.array(centers,dtype=int)
-    #rospy.loginfo("   centers:"+str(centers)+"shape"+str(centers.shape))
-    #rospy.loginfo(thresh1.shape)
-    
-    #thresh1[:,:]=0 #w
-    #for i in range(6):
-    #  thresh1[centers[i,0],centers[i,1]]=255 #b
-    
-    #rospy.sleep(2)
 
     """
     #gauss
@@ -226,14 +223,6 @@ class image_converter:
     #
     # plt.show()
     # print("Done")
-
-    try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(thresh1, "mono8"))
-      #self.image_pub.publish(self.bridge.cv2_to_imgmsg(gray, "mono8"))
-      #self.image_pubC.publish(self.bridge.cv2_to_imgmsg(thresh2, "mono8"))
-      rospy.loginfo("  publishing an image.")
-    except CvBridgeError as e:
-      print(e)
       
 
 def main(args):
