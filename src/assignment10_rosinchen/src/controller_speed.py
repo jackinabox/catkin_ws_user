2e3cf18cbf6e6e1d1ad78f7632d7c47ff82f25bb
jackinabox <adrian.defer@gmail.com>
2019-01-21 12:24:49 +0100

    format

--- src/assignment10_rosinchen/src/controller_speed.py
+++ src/assignment10_rosinchen/src/controller_speed.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
 # --- imports ---
-#import roslib
+# import roslib
 import rospy
-#import sys
-#import cv2
+# import sys
+# import cv2
 import numpy as np
-#from collections import deque
+# from collections import deque
 from std_msgs.msg import UInt8
 from std_msgs.msg import Int16
 from std_msgs.msg import Float32
-#from nav_msgs.msg import Odometry
+# from nav_msgs.msg import Odometry
 from std_msgs.msg import String
 from std_msgs.msg import Bool
 from geometry_msgs.msg import Pose
@@ -18,20 +18,19 @@ from geometry_msgs.msg import Point
 from geometry_msgs.msg import Quaternion
 from sensor_msgs.msg import Image
 
-
 trigger = True
-u_init = 1.0 # initial car speed
-k_p = 0.1 # P controller
-k_d = 0.1 # D controller
+u_init = 1.0  # initial car speed
+k_p = 0.1  # P controller
+k_d = 0.1  # D controller
 curve_treshold = 1
-
 
 actual_speed = None
 actual_speed_dif = None
-target_speed = None # in m/s
-slow_curve=0.5 # ratio of target_speed 0.5=50%
-target_speed_dif = 0. # in m*s^-2
-curve=False
+target_speed = None  # in m/s
+slow_curve = 0.5  # ratio of target_speed 0.5=50%
+target_speed_dif = 0.  # in m*s^-2
+curve = False
+
 
 def waitForTrigger():
     while trigger == False:
@@ -39,6 +38,7 @@ def waitForTrigger():
             "%s: No starting signal received. Waiting for message...",
             rospy.get_caller_id())
         rospy.sleep(0.5)
+
 
 def waitForFirstSpeed():
     while actual_speed == None or actual_speed_dif == None:
@@ -52,27 +52,32 @@ def waitForFirstSpeed():
             rospy.get_caller_id())
         rospy.sleep(0.5)
 
+
 def callbackTargetSpeed(msg):
     global target_speed
     target_speed = msg.data
 
+
 def callbackLineParameter(msg):
     global curve
     m = msg.x
-    #rospy.loginfo("anstieg: "+str(m))
-    store_curve=curve
-    if abs(m)>curve_treshold:
+    # rospy.loginfo("anstieg: "+str(m))
+    store_curve = curve
+    if abs(m) > curve_treshold:
         curve = True
     else:
         curve = False
+
 
 def callback_actualSpeed(msg):
     global actual_speed
     actual_speed = msg.data
 
+
 def callback_actualSpeedDif(msg):
     global actual_speed_dif
     actual_speed_dif = msg.data
+
 
 def callbackTrigger(msg):
     rospy.loginfo("str(msg)")
@@ -80,19 +85,21 @@ def callbackTrigger(msg):
     global trigger
     trigger = msg
 
-#def get_error_array():
+
+# def get_error_array():
 #    return np.array(list(errors))
 
-#def get_latest_error():
+# def get_latest_error():
 #    arr = get_error_array()
 #    return arr[0]
 
-#def get_mean_error():
+# def get_mean_error():
 #    arr = get_error_array()
 #    return np.mean(arr)
 
-def speed_mapping(target): # input [m/s], output [car speed (~300)]
-    return 359.687*target+81.0456
+def speed_mapping(target):  # input [m/s], output [car speed (~300)]
+    return 359.687 * target + 81.0456
+
 
 def control():
     global actual_speed
@@ -102,15 +109,16 @@ def control():
 
     speed = actual_speed
     speed_dif = actual_speed_dif
-    #rospy.loginfo("sub_actualSpeed: %f, %f" % speed, speed_dif)
-    u_t = target_speed + k_p * (target_speed-actual_speed) + k_d * (target_speed_dif-actual_speed_dif)
-    u_t = u_t - curve*slow_curve*target_speed # slow down in curves.
+    # rospy.loginfo("sub_actualSpeed: %f, %f" % speed, speed_dif)
+    u_t = target_speed + k_p * (target_speed - actual_speed) + k_d * (target_speed_dif - actual_speed_dif)
+    u_t = u_t - curve * slow_curve * target_speed  # slow down in curves.
     speed_car = np.clip(speed_mapping(u_t), 0, 600)
-    #rospy.loginfo("publish speed"+str(speed_car))
+    # rospy.loginfo("publish speed"+str(speed_car))
     pub_speed.publish(speed_car)
     pub_logspeed.publish(str(speed_car))
-    #rospy.loginfo("\terror: %d -- steering: %d" % (err, steering))
+    # rospy.loginfo("\terror: %d -- steering: %d" % (err, steering))
     rospy.sleep(0.1)
+
 
 rospy.init_node("controller_speed", anonymous=True)
 
@@ -131,6 +139,3 @@ while trigger and not rospy.is_shutdown():
 
 # spin() simply keeps python from exiting until this node is stopped
 rospy.spin()
-
-
-
