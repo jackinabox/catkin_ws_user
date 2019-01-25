82b9a8373f66cd9b22615e0eb4c2cc57b5e392ba
jackinabox <adrian.defer@gmail.com>
2019-01-25 15:37:14 +0100

    update code

--- src/assignment11_rosinchen/src/setup_values.py
+++ src/assignment11_rosinchen/src/setup_values.py
@@ -4,12 +4,12 @@
 class Setup:
 
 	def __init__(self):
-		self.lookahead_distance = 0.35  # distance = 0.35
+		self.lookahead_distance = 0.45  # distance = 0.35
 		self.carID = 5
 		self.target_speed = 450
-		self.curve_angle = 30
+		self.curve_angle = 25
 		self.slowdown_curve = 0.75
-		self.laneID = 0
+		self.laneID = 1
 		self.logging = False
 		self.gps_offset = [-0.03, -0.015]
 		# rospy.loginfo("initial setup:\n")
